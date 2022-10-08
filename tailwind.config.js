3df7506fd0b03246acd1cd16bd613e22dcc65d85
alimonapour <a.monapoor@gmail.com>
2022-10-09 01:07:50 +0330

    add tailwind css and assets to app

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,32 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
-  ],
+  content: ['./pages/**/*.{js,ts,jsx,tsx}', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Helvetica', 'Roboto', 'Inter', 'sans-serif'],
+        mono: ['Roboto Mono'],
+      },
+      colors: {
+        darkTheme: '#242933',
+        brand: '#4F46E5',
+        midnight: '#121063',
+        metal: '#5E81AC',
+        tahiti: '#3ab7bf',
+        silver: '#ecebff',
+        bubbleGum: '#ff77e9',
+        bermuda: '#78dcca',
+        LightYellow: '#EBCB8B',
+        lightSalmon: '#D08770',
+        LightSkyBlue: '#5E81AC',
+      },
+      listStyleType: {
+        hashtag: '##',
+      },
+    },
+    container: {
+      center: true,
+    },
   },
   plugins: [],
 }

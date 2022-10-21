3a6c70b29758ca74474b0565cbfbd0a65678219d
alimonapour <a.monapoor@gmail.com>
2022-10-21 15:47:36 +0330

    add text color to important title and added background to skills

--- src/components/About.js
+++ src/components/About.js
@@ -4,35 +4,61 @@ export function About() {
       <h1 className=' text-3xl sm:text-4xl font-semibold my-3'>About Me</h1>
       <section className='space-y-3 ml-2 font-medium'>
         <p className='text-xl sm:text-2xl text-left'>
-          I am a Front-End developer with over 4 years of experience in
-          implementing complex web applications, specializing in SPA development
-          utilizing React ecosystem.
+          I am a{' '}
+          <span className='font-semibold text-tahiti '>
+            Front-End developer
+          </span>{' '}
+          with{' '}
+          <span className='font-semibold text-tahiti'>
+            over 4 years of experience{' '}
+          </span>{' '}
+          in implementing complex web applications, specializing in SPA
+          development utilizing{' '}
+          <span className='font-semibold text-tahiti '>React</span> ecosystem.
         </p>
         <p className='text-xl sm:text-2xl text-left'>
-          Furthermore, I am also experienced with Node JS and Express JS. I can
-          also work as a full-stack developer but prefer front-end development
-          that I am very passionate about.
+          Furthermore, I am also experienced with{' '}
+          <span className=' font-semibold text-tahiti '>
+            Node JS and Express JS
+          </span>
+          . I can also work as a full-stack developer but prefer front-end
+          development that I am very passionate about.
         </p>
         <p className='text-xl sm:text-2xl text-left'>
-          Also, as a React Native developer for my freelancing experience, I
-          developed Android and IOS applications, one of which is in the Google
-          Play store and the Apple App Store.
+          Also, as a{' '}
+          <span className='font-semibold text-tahiti'>
+            React Native developer
+          </span>{' '}
+          for my freelancing experience, I developed Android and IOS
+          applications, one of which is in the Google Play store and the Apple
+          App Store.
         </p>
         <p className='text-xl sm:text-2xl text-left'>
-          Besides being passionate, motivated, and hardworking, I also love
-          solving challenges, writing tests, fixing bugs, and learning new
-          things and new technologies. Furthermore, I&apos;m flexible and I
-          embrace new challenges.
+          Besides being <span className='text-tahiti'>passionate</span>,{' '}
+          <span className='text-tahiti'>motivated</span>, and{' '}
+          <span className='text-tahiti'>hardworking</span>, I also love solving
+          challenges, writing tests, fixing bugs, and learning new things and
+          new technologies . Furthermore, I&apos;m{' '}
+          <span className='text-tahiti'>flexible</span> and I embrace{' '}
+          <span className='text-tahiti'>new challenges</span>.
         </p>
         <p className='text-xl sm:text-2xl text-left'>
-          I would describe myself as an industrious, self-motivated,
-          resourceful, and positive person who wants to have a long and
-          successful career in this role and this industry.
+          I would describe myself as an{' '}
+          <span className='text-tahiti'>industrious</span>,{' '}
+          <span className='text-tahiti'>self-motivated</span>,{' '}
+          <span className='text-tahiti'>resourceful</span>, and{' '}
+          <span className='text-tahiti'>positive person</span> who wants to have
+          a long and successful career in this role and this industry.
         </p>
         <p className='text-xl sm:text-2xl text-left'>
-          I am so eager to learn new skills, be proactive and keep up with
-          trends in the development world and evolving tech stack, and my
-          experience makes me an easy partner to work with.
+          I am so eager to <span className='text-tahiti'>learn new skills</span>
+          , <span className='text-tahiti'>be proactive</span> and{' '}
+          <span className='text-tahiti'>
+            keep up with trends in the development
+          </span>{' '}
+          world and <span className='text-tahiti'>evolving tech stack</span>,
+          and my experience makes me{' '}
+          <span className='text-tahiti'>an easy partner</span> to work with.
         </p>
         <p className='text-xl sm:text-2xl text-left'>
           The sport of football is very important to me, and I am also fond of
@@ -43,19 +69,44 @@ export function About() {
           <h3 className='text-xl sm:text-2xl my-3 text-left'>
             Here are a few technologies I’ve been working with recently:
           </h3>
-          <ul className='grid lg:grid-cols-3 sm:grid-cols-2 list-disc list-inside text-xl sm:text-2xl py-3 ml-3'>
-            <li>JavaScript (ES6+)</li>
-            <li>TypeScript</li>
-            <li>React JS(Hooks) </li>
-            <li>Next JS</li>
-            <li>React Native</li>
-            <li>Redux JS</li>
-            <li>HTML5 && CSS3</li>
-            <li>Node JS (Express JS)</li>
-            <li>Jest</li>
-            <li>Cypress</li>
-            <li>React Testing Library</li>
-            <li>Tailwind CSS</li>
+          <ul className='grid lg:grid-cols-3 sm:grid-cols-2  text-xl sm:text-2xl py-3 font-semibold '>
+            <li className='bg-LightDark p-2.5 my-1 sm:mx-1 rounded ring-metal ring-2 text-center '>
+              JavaScript (ES6+)
+            </li>
+            <li className='bg-LightDark p-2.5 my-1 sm:mx-1 rounded ring-metal ring-2 text-center'>
+              TypeScript
+            </li>
+            <li className='bg-LightDark p-2.5 my-1 sm:mx-1 rounded ring-metal ring-2 text-center'>
+              React JS(Hooks){' '}
+            </li>
+            <li className='bg-LightDark p-2.5 my-1 sm:mx-1 rounded ring-metal ring-2 text-center'>
+              Next JS
+            </li>
+            <li className='bg-LightDark p-2.5 my-1 sm:mx-1 rounded ring-metal ring-2 text-center'>
+              React Native
+            </li>
+            <li className='bg-LightDark p-2.5 my-1 sm:mx-1 rounded ring-metal ring-2 text-center'>
+              HTML5 && CSS3
+            </li>
+            <li className='bg-LightDark p-2.5 my-1 sm:mx-1 rounded ring-metal ring-2 text-center'>
+              Node JS (Express JS)
+            </li>
+            <li className='bg-LightDark p-2.5 my-1 sm:mx-1 rounded ring-metal ring-2 text-center'>
+              Redux JS
+            </li>
+
+            <li className='bg-LightDark p-2.5 my-1 sm:mx-1 rounded ring-metal ring-2 text-center'>
+              Jest
+            </li>
+            <li className='bg-LightDark p-2.5 my-1 sm:mx-1 rounded ring-metal ring-2 text-center'>
+              Cypress
+            </li>
+            <li className='bg-LightDark p-2.5 my-1 sm:mx-1 rounded ring-metal ring-2 text-center'>
+              React Testing Library
+            </li>
+            <li className='bg-LightDark p-2.5 my-1 sm:mx-1 rounded ring-metal ring-2 text-center'>
+              Tailwind CSS
+            </li>
           </ul>
         </div>
       </section>
